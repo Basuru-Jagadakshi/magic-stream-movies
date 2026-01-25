a7c14ec2ec982701c8686028701ce48da398ff7e
Basuru-Jagadakshi <jagadakshibasuru@gmail.com>
2026-01-25 17:58:56 +0530

    first day end commit

---
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -0,0 +1,91 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Basuru-Jagadakshi/magic-stream-movies/Server/MagicStreamMoviesServer/database"
+	"github.com/Basuru-Jagadakshi/magic-stream-movies/Server/MagicStreamMoviesServer/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"github.com/go-playground/validator/v10"
+
+	"github.com/gin-gonic/gin"
+)
+
+var movieCollection *mongo.Collection = database.OpenCollections("movies")
+
+var validate = validator.New()
+
+func GetMovies() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movies[]models.Movie
+		
+		cursor, err :=  movieCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
+		}
+		defer cursor.Close(ctx)
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
+		}
+
+		c.JSON(http.StatusOK, movies)
+	}
+}
+
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error":"Movie ID is required"})
+			return 
+		}
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id":movieID}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error":"Movie not found"})
+			return 
+		}
+
+		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":"INvalid input"})
+			return 
+		}
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":"Validations failed", "details":err.Error()})
+			return 
+		}
+		
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"Failed to add movie"})
+			return 
+		}
+
+		c.JSON(http.StatusCreated, result)
+	}
+}
